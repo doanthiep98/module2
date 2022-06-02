f3310c6369ba2626343aa16cf0ecfd6d54693ec9
Thiep <doanthiep98@gmail.com>
2022-06-02 14:26:42 +0700

    first two

---
+++ test01.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class test01 {
+    public static void main(String[] args) {
+        Date now= new Date();
+        System.out.print("now is " +now);
+    }
+}
---
+++ values.java
@@ -0,0 +1,17 @@
+public class values {
+    public static void main(String[] args) {
+        int a=10;
+        float b= 18.7f;
+        double c= 19.8;
+        boolean d= false;
+        String e="cat";
+        char f= 'm';
+        System.out.println(a);
+         System.out.println(b);
+         System.out.println(c);
+         System.out.println(d);
+         System.out.println(e);
+         System.out.println(f);
+
+    }
+}
