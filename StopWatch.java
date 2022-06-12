df8de96ee018c974c732265e9d34271865d0d879
Thiep <doanthiep98@gmail.com>
2022-06-12 22:55:45 +0700

    com

---
+++ StopWatch.java
@@ -0,0 +1,21 @@
+package Java;
+
+public class StopWatch {
+        double startTime;
+        double endTime;
+
+        public StopWatch() {
+        }
+
+        public void getStartTime() {
+            startTime = System.currentTimeMillis();
+        }
+
+        public void getEndTime() {
+            endTime = System.currentTimeMillis();
+        }
+
+        public double getElapsedTime() {
+            return endTime - startTime;
+        }
+}
